5d9a792f575c9df33354186ed0995f7c72594853
choisungwook <kgg1959@naver.com>
2021-10-21 20:38:13 +0900

    MAIN-27 jenkinsapi job트리거 호출

--- src/main/java/com/srm/projectmanager/jenkins_trigger/JenkinsController.java
+++ src/main/java/com/srm/projectmanager/jenkins_trigger/JenkinsController.java
@@ -2,9 +2,11 @@ package com.srm.projectmanager.jenkins_trigger;
 
 import com.srm.projectmanager.application.ApplicationEntity;
 import com.srm.projectmanager.application.ApplicationRepos;
+import com.srm.projectmanager.jenkins_trigger.dto.RequestJobTriggerDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,17 +24,11 @@ public class JenkinsController {
     @Autowired
     ApplicationRepos applicationRepos;
 
-    @Value("${jenkins.endpoint}")
-    private String endpoint;
+    @Value("${endpoint.jenkins_api}")
+    private String jenkinsapi_endpoint;
 
-    @Value("${jenkins.user}")
-    private String jenkins_user;
-
-    @Value("${jenkins.token}")
-    private String jenkins_token;
-
-    @Value("${jenkins.testapi}")
-    private String test_url;
+    @Value("${api.jenkins.job.trigger}")
+    private String jenkinsapi_jobtrigger;
 
     @PostMapping("/trigger/job/{id}")
     @Transactional(readOnly = true)
@@ -46,20 +42,17 @@ public class JenkinsController {
 
         String response = webClientBuilder.build()
             .post()
-            .uri(getTriggerUrl(find_application))
-            .headers(header -> header.setBasicAuth(jenkins_user, jenkins_token))
+            .uri(jenkinsapi_endpoint + jenkinsapi_jobtrigger)
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(
+                new RequestJobTriggerDTO(find_application.getJenkins_job_url(), find_application.getToken())
+            )
             .retrieve()
             .bodyToMono(String.class)
             .block();
 
         // todo response fail
-
         log.info("----------------- jenkins job 실행API 종료 -----------------------");
-
         return "done";
-    }
-
-    private String getTriggerUrl(ApplicationEntity applicationEntity){
-        return endpoint + applicationEntity.getJenkins_job_url() + "/build?token=" + applicationEntity.getToken();
     }
 }
